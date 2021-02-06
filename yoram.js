a5afb4c47b1979709fc4ecf11dfa2d8561bd67fa
etyoungsu <etyoungsu@ajou.ac.kr>
2021-02-06 17:39:33 +0900

    Ask Your name

---
+++ yoram.js
@@ -0,0 +1,45 @@
+const form = document.querySelector(".js-name"),
+    input = form.querySelector("input"),
+    greet = document.querySelector(".js-greeting");
+
+const USER_LS = "userName",
+    SHOWING = "showing";
+
+
+function saveName(text) {
+    localStorage.setItem(USER_LS, text);
+}
+
+function paintName(text) {
+    form.classList.remove(SHOWING);
+    greet.classList.add(SHOWING);
+    greet.innerText = `Hello ${text}`;
+}
+
+function handler(event) {
+    event.preventDefault();
+    const value = input.value;
+    paintName(value);
+    saveName(value);
+}
+
+function waitForName() {
+    form.classList.add(SHOWING);
+    form.addEventListener("submit", handler);
+}
+
+
+function loadName() {
+    const userName = localStorage.getItem(USER_LS);
+    if (userName === null) {
+        waitForName();
+    }
+    else
+        paintName(userName);
+}
+
+function init() {
+    loadName();
+}
+
+init();
